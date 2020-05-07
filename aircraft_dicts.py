22b3ad15dae15845378c5d544b2f4f4e93c76b83
Evan <64546723+TheEBUG@users.noreply.github.com>
2020-05-07 13:09:13 -0700

    Created base aircraft/helo dictionaries as strings
    -dicts based on aircraft sizes and classifications
    -separate mil and helo dicts
    -all based on strings as start, looking to switch into classes

---
+++ aircraft_dicts.py
@@ -0,0 +1,81 @@
+# set defs, dicts and classes
+
+single_piston_dict = {
+    "Cessna": ["150", "152", "172", "182", "206", "350", "400", "TTX"],
+    "Piper": ["Archer", "Arrow", "Challenger", "Cherokee", "Malibu", "Cub", "Saratoga"],
+    "Mooney": ["M10", "M20", "Bravo"],
+    "Cirrus": ["SR-20", "SR-22"],
+    "Beechcraft": ["Bonanza"],
+    "Grumman": ["AA-1", "AA-5"],
+    "Glasair": ["Glasair", "Sportsman"],
+    "DeHavilland": ["Beaver"],
+    "Other": ["Other"],
+}
+
+multi_piston_dict = {
+    "Cessna": ["300 Series", "400 series"],
+    "Piper": ["Seneca", "Aztec", "Navajo", "Twin Comanche", "Seminole"],
+    "Beechcraft": ["Baron"],
+    "Aerostar": ["Aerostar"],
+    "Diamond": ["DA Series"],
+    "Other": ["Other"],
+}
+
+small_turboprop_dict = {
+    "Piper": ["Malibu"],
+    "Mitsubishi": ["MU-2"],
+    "TBM": ["850", "950"],
+    "Other": ["Other"],
+}
+
+large_turboprop_dict = {
+    "Cessna": ["Caravan", "Grand Caravan", "Grand Caravan Float"],
+    "DeHavilland": ["Beaver Float", "Otter Float"],
+    "Pilatus": ["PC-12"],
+    "Kodiak": ["Kodiak"],
+    "Beechcraft": ["King Air"],
+    "Metroliner": ["Metroliner"],
+    "Other": ["Other"],
+}
+
+small_jet_dict = {
+    "Cirrus": ["Cirrus Jet"],
+    "Lear": ["30 Series", "40 Series"],
+    "Cessna": ["Citation Mustang"],
+    "Beechcraft": ["Premier 1", "BeechJet"],
+    "Embraer": ["Phenom 100"],
+    "Hawker": ["400 Series"],
+    "Other": ["Other"],
+}
+
+medium_jet_dict = {
+    "Cessna": ["Citation X", "Sovereign", "Latitude", "Other Citation"],
+    "Falcon": ["20", "50"],
+    "Hawker": ["700 Series", "800 Series", "900 Series"],
+    "Challenger": ["300 Series"],
+    "Gulfstream": ["Gulf III"],
+    "Embraer": ["Phenom 300"],
+    "Other": ["Other"],
+}
+
+large_jet_dict = {
+    "Falcon": ["900 Series", "2000 Series", "6X", "7X", "8X"],
+    "Challenger": ["600 Series", "700 Series"],
+    "Gulfstream": ["G4", "G450", "G5", "G550", "G6", "G650", "G7", "G750"],
+    "Bombardier": ["Global Express"],
+    "Boeing": ["BBJ", "737"],
+}
+
+military_dict = {
+    "USN": ["F-18", "P-8", "Osprey", "SeaHawk", "Sea Dragon"],
+    "USA": ["Chinook", "Black Hawk"],
+    "USAF": ["F-16", "F-22"],
+    "USCG": ["Jay Hawk", "Dolphin"],
+    "Other": ["Other"],
+}
+
+helos_dict = {
+    "Bell": ["200 Series", "400 Series"],
+    "Robinson": ["R22", "R44", "R66"],
+    "Sikorsky": ["S76", "S92"],
+}
